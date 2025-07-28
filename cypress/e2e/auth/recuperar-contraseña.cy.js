518b977866c69ffda8d5a55c449627ee131ff8c3
matiasleonel28 <157613882+matiasleonel28@users.noreply.github.com>
2025-07-28 18:14:13 -0300

    nuevo archivo:   cypress/fixtures/authData.json

--- cypress/e2e/auth/recuperar-contraseña.cy.js
+++ cypress/e2e/auth/recuperar-contraseña.cy.js
@@ -7,25 +7,31 @@ describe('Recuperar Contraseña - Validaciones', () => {
   describe('Tests Positivos', () => {
 
     it('[TC-REC-03] Envío exitoso con email válido registrado', () => {
-      cy.get('input[type="email"]').clear().type('3ypsmhh8c5@zudpck.com');
-      cy.contains('button', 'Enviar').click();
-      cy.contains('Se ha enviado un correo para restablecer la contraseña').should('exist');
-      cy.wait(1000);
-      cy.url().should('include', '/auth/login');
+      cy.fixture('authData').then((data) => {
+        cy.get('input[type="email"]').clear().type(data.emailValido);
+        cy.contains('button', 'Enviar').click();
+        cy.contains('Se ha enviado un correo para restablecer la contraseña').should('exist');
+        cy.url().should('include', '/auth/login');
+      });
     });
 
     it('[TC-REC-04] Envío con email válido pero no registrado', () => {
-      cy.get('input[type="email"]').clear().type('notfound999@qa.com');
-      cy.contains('button', 'Enviar').click();
-      cy.contains('Se ha enviado un correo para restablecer la contraseña').should('exist');
-      cy.url().should('include', '/auth/login');
+      cy.fixture('authData').then((data) => {
+        cy.get('input[type="email"]').clear().type(data.emailNoRegistrado);
+        cy.contains('button', 'Enviar').click();
+        cy.contains('Se ha enviado un correo para restablecer la contraseña').should('exist');
+        cy.url().should('include', '/auth/login');
+      });
     });
 
+
     it('[TC-REC-06] Confirmación visible tras envío exitoso', () => {
-      cy.get('input[type="email"]').clear().type('3ypsmhh8c5@zudpck.com');
-      cy.contains('button', 'Enviar').click();
-      cy.contains('Se ha enviado un correo para restablecer la contraseña').should('be.visible');
-      cy.url().should('include', '/auth/login');
+      cy.fixture('authData').then((data) => {
+        cy.get('input[type="email"]').clear().type(data.emailValido);
+        cy.contains('button', 'Enviar').click();
+        cy.contains('Se ha enviado un correo para restablecer la contraseña').should('be.visible');
+        cy.url().should('include', '/auth/login');
+      });
     });
 
     it('[TC-REC-07] Validar acceso de retorno a Login desde la pantalla actual', () => {
@@ -43,12 +49,13 @@ describe('Recuperar Contraseña - Validaciones', () => {
     });
 
     it('[TC-REC-02] Validación de envío con email de formato inválido', () => {
-      const emailsInvalidos = ['2', '☻'];
-      emailsInvalidos.forEach((input) => {
-        cy.get('input[type="email"]').clear().type(input);
-        cy.contains('button', 'Enviar').click();
-        cy.contains(`Incluye un signo "@" en la dirección de correo electrónico. La dirección "${input}" no incluye el signo "@"`).should('exist');
-        cy.contains('Se ha enviado un correo para restablecer la contraseña').should('not.exist');
+      cy.fixture('authData').then((data) => {
+        data.emailsInvalidos.forEach((input) => {
+          cy.get('input[type="email"]').clear().type(input);
+          cy.contains('button', 'Enviar').click();
+          cy.contains(`Incluye un signo "@" en la dirección de correo electrónico`).should('exist');
+          cy.contains('Se ha enviado un correo para restablecer la contraseña').should('not.exist');
+        });
       });
     });
 
